e6c4995e40e730d755fea3b57d06f2b6b711b279
Ahmed Hassan <93686680+AhmedHassan1241@users.noreply.github.com>
2025-06-23 03:31:27 +0300

    Update Skills Order

---
+++ .history/src/components/5-skills/skillsData_20250623033120.js
@@ -0,0 +1,34 @@
+
+import { FaHtml5 } from "react-icons/fa6";
+import { IoLogoCss3 } from "react-icons/io";
+import { RiJavascriptLine } from "react-icons/ri";
+import { FaBootstrap } from "react-icons/fa";
+import { TbBrandTypescript } from "react-icons/tb";
+import { FaGitAlt } from "react-icons/fa";
+import { GrReactjs } from "react-icons/gr";
+import { DiNodejs } from "react-icons/di";
+import { SiExpress } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { SiPhp } from "react-icons/si";
+import { SiMysql } from "react-icons/si";
+import { TbBrandLaravel } from "react-icons/tb";
+
+
+
+
+export const skillsData=[
+    {id:"1",icon:FaHtml5,name:"HTML 5"},
+    {id:"2",icon:IoLogoCss3,name:"CSS 3"},
+    {id:"3",icon:RiJavascriptLine,name:"JavaScript"},
+    {id:"4",icon:FaBootstrap,name:"Bootstrap"},
+    {id:"5",icon:TbBrandTypescript,name:"TypeScript"},
+    {id:"6",icon:FaGitAlt,name:"Git"},
+    {id:"7",icon:GrReactjs,name:"React Js"},
+    {id:"8",icon:DiNodejs,name:"Node Js"},
+    {id:"9",icon:SiExpress,name:"Express"},
+    {id:"10",icon:SiMongodb,name:"MongoDB"}
+    {id:"11",icon:SiPhp,name:"PHP"},
+    {id:"12",icon:SiMysql,name:"MySQL"},
+    {id:"13",icon:TbBrandLaravel,name:"Laravel"},
+
+]
